de2d2aaf53fde1976385861564b201e7c56d5ce5
manmolecular <regwebghost@yandex.ru>
2017-05-17 21:18:03 +0700

    Функции для работы с битами

--- arithmetic_coding/Source.cpp
+++ arithmetic_coding/Source.cpp
@@ -187,14 +187,42 @@ string encode_text(int *freq, string text, string abc)
 	return code;
 }
 
-int to_int(string text)
+int to_int(string text, int _pos)
 {
 	int n = 0;
-	for (int i = 0; i < sizeof(short)*8; i++) {
+	for (int i = _pos; i < sizeof(short)*8 + _pos; i++) 
+	{
 		n <<= 1;
 		n |= text[i] - '0';
 	}
 	return n;
+}
+
+string to_bits_16(int _value)
+{
+	bitset<16> a(_value); //convent number into bit array
+	string mystring = a.to_string<char, char_traits<char>, allocator<char> >();
+	return mystring;
+}
+
+int add_bit(int buffer, int value)
+{
+	/* Создаем битсет объекты */
+	bitset<16> a(value);
+	bitset<16> b(buffer);
+
+	/* Проверяем первый бит в буффере (если 1)*/
+	bool check = b.test(0);
+	if (check)
+	{
+		a.set(0);
+	}
+	else
+	{
+		a.reset(0);
+	}
+	value = (unsigned short int)(a.to_ulong());
+	return value;
 }
 
 int decode(int *freq, string text, string abc)
@@ -203,61 +231,123 @@ int decode(int *freq, string text, string abc)
 	int _high[500];
 	_low[0] = _low_edge;
 	_high[0] = _high_edge;
+
 	int range = 0;
 	int cum = 0;
 	int del = freq[abc.size() - 1];
-	int current = 1;
-	unsigned short int value = to_int(text);			//Забираем 2 байта
-	int bits_to_go = 0;
-	int buffer = 0;
-	for (int i = 0; i < text.size(); i++)
+
+	unsigned short int value = to_int(text, 0);		// Забираем 16 бит в value
+
+	int bits_to_go = 0;		// Сколько бит осталось в буфере
+	int buffer = 0;			// Сам буфер битов
+	int count_taken = 1;	// Сколько раз забрали в буфер (опр. позицию считывания)
+
+	for (int i = 1; i < text.size(); i++)
 	{
 		range = (_high[i - 1] - _low[i - 1]) + 1;
 		cum = (((value - _low[i - 1]) + 1) * del - 1) / range;
-		for (int i = 1; freq[i] > cum;)
+		
+		int symbol;
+		for (symbol = 1; freq[i] > cum; symbol++);
+
+		_low[i] = _low[i - 1] + (range * freq[i - 1]) / del - 1;
+		_high[i] = _low[i - 1] + (range * freq[i]) / del;
+
+		if (debug_mode)
 		{
-			get_next_symbol(text, abc, i, &current);
-			i += 1;
-			_low[i] = _low[i - 1] + (range * freq[i - 1]) / del - 1;
-			_high[i] = _low[i - 1] + (range * freq[i]) / del;
+			cout << "Symbol is: " << abc.at(symbol) << endl;
+			cout << "Value is: " << value << endl;
 		}
+
 		for (;;)
 		{
 			if (_high[i] >= half)
 			{
 				if (_low[i] >= half)
 				{
-					value -= half; _low[i] -= half; _high[i] -= half;
+					value -= half;
+					_low[i] -= half;
+					_high[i] -= half;
 				}
 				else if(_low[i] >= first_qtr && _high[i] < third_qtr)
 				{
-					value -= first_qtr; _low[i] -= first_qtr; _high[i] -= first_qtr;
+					value -= first_qtr;
+					_low[i] -= first_qtr;
+					_high[i] -= first_qtr;
 				}
 				else break;
 			}
 			_low[i] = 2 * _low[i];
 			_high[i] = 2 * _high[i] + 1;
 
-			/* Сдвиг влево и добавление бита */
-			fstream input;
-			input.open(filename_input, ios::in);
+			/* Заполнение буфера */
 			if (bits_to_go == 0)
 			{
-				input.read((char *)&buffer, sizeof(unsigned short int));
+				buffer = to_int(text, 16 * count_taken);
 				bits_to_go = 16;
 			}
-			bool new_bit = buffer & 1;
-			buffer >>= 1;
+			value = add_bit(value*2, buffer);
+			buffer = buffer >> 1;
 			bits_to_go--;
-			value = (value << 1) + new_bit;
 		}
 	}
 	return value;
 }
 
+void test_function(string text)
+{
+	cout << endl << "TEST FUNCTION: " << endl;
+
+	unsigned short int value = to_int(text, 0);			//Забираем 2 байта
+	unsigned short int buffer = to_int(text, 16);
+	int bits_to_go = 0;
+
+
+	string _value = to_bits_16(value);
+	string _buffer = to_bits_16(buffer);
+	cout << "Value (0-15): " << _value << endl;
+	cout << "Buffer (16-31): " << _buffer << endl << endl;
+
+	cout << "ADD BIT WITH SLIDE: " << endl;
+	cout << "value: " << _value << endl;
+	value *= 2;
+	_value = to_bits_16(value);
+	cout << "value * 2: " << _value << endl;
+
+	/* Создаем битсет объекты */
+	bitset<16> a(value);
+	bitset<16> b(buffer);
+
+	/* Проверяем первый бит в буффере (если 1)*/
+	bool check = b.test(0);
+	if (check)
+	{
+		a.set(0);
+	}
+	else
+	{
+		a.reset(0);
+	}
+	value = (unsigned short int)(a.to_ulong());
+	_value = to_bits_16(value);
+	cout << "value *2 + new_bit: " << _value << endl;
+
+	//cout << "SLIDE: " << endl;
+	//cout << "Buffer (16-31): " << _buffer << endl;
+	//buffer *= 2;
+	//_buffer = to_bits_16(buffer);
+	//cout << "Buffer (16-31) * 2: " << _buffer << endl << endl;
+	//bitset<16> c(_buffer);
+	//c.reset(15);
+	//buffer = (unsigned short int)(c.to_ulong());
+	//_buffer = to_bits_16(buffer);
+	//cout << "Buffer (16-31) LAST: " << _buffer << endl << endl;
+}
+
 int main()
 {
-	string text = get_file(filename_input);
+	//string text = get_file(filename_input);
+	string text = "Hello, world!";
 	string abc = get_abc(text);
 	int *freq = new int[abc.size()];
 	get_frequency(text, freq, abc);
@@ -275,6 +365,8 @@ int main()
 
 	string code = encode_text(freq, text, abc);
 	output_file(filename_output, code);
+
+	//test_function(code);
 
 	int temp = decode(freq, code, abc);
 	cout << temp << endl;
