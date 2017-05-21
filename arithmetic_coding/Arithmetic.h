38c0da517fdbb8a1e5b2d13defde6baae41e0443
manmolecular <regwebghost@yandex.ru>
2017-05-22 00:37:31 +0700

    Класс арифметического кодирования

---
+++ arithmetic_coding/Arithmetic.h
@@ -0,0 +1,373 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+
+#define filename_input "input.txt"
+#define filename_output "output.txt"
+#define debug_mode false
+#define _low_edge 0
+#define _high_edge 65535
+#define EOF_symbol '-'
+#define symbol_break '\0'
+#define test_count 10000
+#define length_of_string 100
+
+using namespace std;
+
+class arithmetic
+{
+	private:
+		const int first_qtr = _high_edge / 4 + 1;
+		const int half = 2 * first_qtr;
+		const int third_qtr = 3 * first_qtr;
+		string text;
+		string abc;
+		string encode;
+		string decode;
+		int *freq;
+	public:
+		void show_table();
+		string get_encode();
+		string get_decode();
+		void get_file(string filename);
+		void input(string input);
+		void get_abc();
+		void get_frequency();
+		void get_next_symbol(int i, int *current);
+		string write_bits(bool bit, int bits_to_foll);
+		void output_file(string filename);
+		void encode_text();
+		int to_int(int _pos);
+		string to_bits_16(int _value);
+		int add_bit(int value, int count_taken, bool &flag);
+		void decode_text();
+};
+
+void arithmetic::show_table()
+{
+	if (debug_mode)
+	{
+		cout << text << endl;
+		cout << abc << endl;
+		for (int i = 0; i < abc.size(); i++)
+		{
+			cout << abc[i] << " - " << freq[i] << endl;
+		}
+		cout << endl;
+	}
+}
+
+string arithmetic::get_decode()
+{
+	return decode;
+}
+
+string arithmetic::get_encode()
+{
+	return encode;
+}
+
+void arithmetic::input(string input)
+{
+	text = input;
+}
+
+/* Помещение текста из файла в string text */
+void arithmetic::get_file(string filename)
+{
+	fstream input;
+	input.open(filename, ios::in);
+	while (!input.eof())
+	{
+		string temp;
+		getline(input, temp);
+		if (!input.eof())
+		{
+			temp += '\n';
+		}
+		text += temp;
+	}
+	input.close();
+}
+
+/* Создание алфавита */
+void arithmetic::get_abc()
+{
+	/* Добавить символ - в начало*/
+	bool exit_flag = 0;
+	abc.push_back(EOF_symbol);
+	for (int i = 0; i < text.size(); i++)
+	{
+		exit_flag = 0;
+		for (int j = 0; j < abc.size(); j++)
+		{
+			if (abc[j] == text[i])						//Если в нашем алфавите уже есть такая буква
+			{
+				exit_flag = 1;							//Выходим, если нашли такую букву
+				break;
+			}
+		}
+		if (exit_flag == 1)								//Если мы находили букву ранее
+		{
+			continue;									//Переходим на следующую итерацию для след. буквы
+		}
+		else											//Если буква найдена не была
+		{
+			abc.push_back(text[i]);
+			continue;
+		}
+	}
+	//sort(abc.begin()+1, abc.end());
+}
+
+/* Получение массива частоты использования символов */
+void arithmetic::get_frequency()
+{
+	freq = new int[abc.size()];
+	for (int i = 0; i < abc.size(); i++)
+	{
+		freq[i] = 0;
+	}
+	for (int j = 0; j < abc.size(); j++)
+	{
+		for (int i = 0; i < text.size(); i++)
+		{
+			if (abc[j] == text[i])
+			{
+				freq[j]++;
+			}
+		}
+		if (j > 0)
+		{
+			freq[j] += freq[j - 1];
+		}
+	}
+}
+
+/* Возвращаем номер символа в алфавите */
+void arithmetic::get_next_symbol(int i, int *current)
+{
+	/* Индекс символа из текста в алфавите */
+	bool exit = false;
+	for (i; i < text.size() && !exit; i++)
+	{
+		char temp_text = text[i];
+		for (int j = 0; j < abc.size(); j++)
+		{
+			char temp_abc = abc[j];
+			if (temp_text == temp_abc)
+			{
+				*current = j;
+				exit = true;
+				break;
+			}
+		}
+	}
+}
+
+/* Запись битов */
+string arithmetic::write_bits(bool bit, int bits_to_foll)
+{
+	string temp;
+	temp += to_string(bit);
+	while (bits_to_foll > 0)
+	{
+		temp += to_string(!bit);
+		bits_to_foll -= 1;
+	}
+	return temp;
+}
+
+/* Запись в файл */
+void arithmetic::output_file(string filename)
+{
+	fstream file;
+	file.open(filename, ios::out);
+	for (int i = 0; i < text.size(); i++)
+	{
+		file << text[i];
+	}
+	file.close();
+}
+
+void arithmetic::encode_text()
+{
+	int mass_size = text.length();
+	unsigned short int *_low = new unsigned short int[mass_size];
+	unsigned short int *_high = new unsigned short int[mass_size];
+	_low[0] = _low_edge;
+	_high[0] = _high_edge;
+	int current = 1;	/* Какой элемент берём */
+	int i = 0;			/* Где находимся */
+	unsigned int range = 0;
+	/* Del - последняя накопленная частота */
+	int del = freq[abc.size() - 1];
+	int bits_to_foll = 0;
+	string code = "";
+
+	while (i < text.length())
+	{
+		get_next_symbol(i, &current);
+		i += 1;
+
+		range = _high[i - 1] - _low[i - 1] + 1;
+		_low[i] = _low[i - 1] + (range*freq[current - 1]) / del;
+		_high[i] = _low[i - 1] + (range*freq[current]) / del - 1;
+		if (debug_mode)
+		{
+			cout << "[" << _low[i] << "; " << _high[i] << ")" << endl;
+		}
+
+		for (;;)
+		{
+			if (_high[i] < half)
+			{
+				code += write_bits(0, bits_to_foll);
+				bits_to_foll = 0;
+			}
+			else if (_low[i] >= half)
+			{
+				code += write_bits(1, bits_to_foll);
+				bits_to_foll = 0;
+				_low[i] -= half;
+				_high[i] -= half;
+			}
+			else if (_low[i] >= first_qtr && _high[i] < third_qtr)
+			{
+				bits_to_foll += 1;
+				_low[i] -= first_qtr;
+				_high[i] -= first_qtr;
+			}
+			else break;
+			_low[i] = 2 * _low[i];
+			_high[i] = 2 * _high[i] + 1;
+		}
+	}
+	if (debug_mode)
+	{
+		cout << endl << code << endl;
+	}
+	encode = code;
+}
+
+int arithmetic::to_int(int _pos)
+{
+	int n = 0;
+	for (int i = _pos; i < sizeof(short) * 8 + _pos; i++)
+	{
+		n <<= 1;
+		n |= encode[i] - '0';
+	}
+	return n;
+}
+
+string arithmetic::to_bits_16(int _value)
+{
+	bitset<16> a(_value); //convent number into bit array
+	string mystring = a.to_string<char, char_traits<char>, allocator<char> >();
+	return mystring;
+}
+
+int arithmetic::add_bit(int value, int count_taken, bool & flag)
+{
+	/* Создаем битсет объекты */
+	bitset<16> a(value);
+
+	/* Проверяем первый бит в буффере (если 1)*/
+	if (flag == 1)
+	{
+		a.reset(0);
+	}
+	else if (count_taken >= encode.length())
+	{
+		a.set(0);
+		flag = 1;
+	}
+	else if (encode[count_taken] == '1')
+	{
+		a.set(0);
+	}
+	else if (encode[count_taken] == '0')
+	{
+		a.reset(0);
+	}
+	value = (unsigned short int)(a.to_ulong());
+	return value;
+}
+
+void arithmetic::decode_text()
+{
+	string decode_text = "";
+	int mass_size = text.length();
+	unsigned short int *_low = new unsigned short int[mass_size];
+	unsigned short int *_high = new unsigned short int[mass_size];
+	_low[0] = _low_edge;
+	_high[0] = _high_edge;
+
+	unsigned int range = 0;
+	unsigned int cum = 0;
+	int del = freq[abc.size() - 1];
+
+	unsigned short int value = to_int(0);		// Забираем 16 бит в value
+	int count_taken = 16;
+
+	bool flag = 0;
+
+	for (int i = 1;; i++)
+	{
+		range = (_high[i - 1] - _low[i - 1]) + 1;
+		cum = (((value - _low[i - 1]) + 1) * del - 1) / range;
+
+		int symbol;
+		for (symbol = 1; freq[symbol] <= cum; symbol++); //Другой знак
+
+		_low[i] = _low[i - 1] + (range * freq[symbol - 1]) / del;
+		_high[i] = _low[i - 1] + (range * freq[symbol]) / del - 1;
+
+		decode_text += abc[symbol];
+		if (debug_mode)
+		{
+			cout << "Symbol is: " << abc.at(symbol) << endl;
+			cout << "Value is: " << value << endl;
+			cout << "Current string is: " << decode_text << endl << endl;
+		}
+
+		if (abc[symbol] == symbol_break)
+		{
+			decode = decode_text;
+			return;
+		}
+
+		for (;;)
+		{
+			if (_high[i] >= half)
+			{
+				if (_low[i] >= half)
+				{
+					value -= half;
+					_low[i] -= half;
+					_high[i] -= half;
+				}
+				else if (_low[i] >= first_qtr && _high[i] < third_qtr)
+				{
+					value -= first_qtr;
+					_low[i] -= first_qtr;
+					_high[i] -= first_qtr;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			_low[i] = 2 * _low[i];
+			_high[i] = 2 * _high[i] + 1;
+			value = add_bit(2 * value, count_taken, flag);
+			count_taken++;
+		}
+	}
+}
--- arithmetic_coding/Source.cpp
+++ arithmetic_coding/Source.cpp
@@ -1,370 +1,10 @@
-﻿#include <string>
-#include <fstream>
-#include <iostream>
-#include <algorithm>
-#include <bitset>
-#include <cmath>
+﻿#include "Arithmetic.h"
 #define filename_input "input.txt"
 #define filename_output "output.txt"
-#define debug_mode false
-#define _low_edge 0
-#define _high_edge 65535
-#define EOF_symbol '-'
 #define symbol_break '\0'
-#define test_count 10000
+#define test_count 1000
 #define length_of_string 100
 using namespace std;
-
-const int first_qtr = _high_edge/4 + 1;
-const int half = 2 * first_qtr;
-const int third_qtr = 3 * first_qtr;
-
-/* Помещение текста из файла в string text */
-string get_file(string filename)
-{
-	string text;
-	fstream input;
-	input.open(filename, ios::in);
-	while (!input.eof())
-	{
-		string temp;
-		getline(input, temp);
-		if (!input.eof())
-		{
-			temp += '\n';
-		}
-		text += temp;
-	}
-	input.close();
-	return text;
-}
-
-/* Создание алфавита */
-string get_abc(string text)
-{
-	/* Добавить символ - в начало*/
-	string abc;
-	bool exit_flag = 0;
-	abc.push_back(EOF_symbol);
-	for (int i = 0; i < text.size(); i++)
-	{
-		exit_flag = 0;
-		for (int j = 0; j < abc.size(); j++)
-		{
-			if (abc[j] == text[i])						//Если в нашем алфавите уже есть такая буква
-			{
-				exit_flag = 1;							//Выходим, если нашли такую букву
-				break;
-			}
-		}
-		if (exit_flag == 1)								//Если мы находили букву ранее
-		{
-			continue;									//Переходим на следующую итерацию для след. буквы
-		}
-		else											//Если буква найдена не была
-		{
-			abc.push_back(text[i]);
-			continue;
-		}
-	}
-	//sort(abc.begin()+1, abc.end());
-	return abc;
-}
-
-/* Получение массива частоты использования символов */
-void get_frequency(string text, int *frequencies, string abc)
-{
-	for (int i = 0; i < abc.size(); i++)
-	{
-		frequencies[i] = 0;
-	}
-	for (int j = 0; j < abc.size(); j++)
-	{
-		for (int i = 0; i < text.size(); i++)
-		{
-			if (abc[j] == text[i])
-			{
-				frequencies[j]++;
-			}
-		}
-		if (j > 0)
-		{
-			frequencies[j] += frequencies[j - 1];
-		}
-	}
-}
-
-void get_next_symbol(string text, string abc, int i, int *current)
-{
-	/* Возвращаем номер символа в алфавите */
-	/* Индекс символа из текста в алфавите */
-	bool exit = false;
-	for (i; i < text.size() && !exit; i++)
-	{
-		char temp_text = text[i];
-		for (int j = 0; j < abc.size(); j++)
-		{
-			char temp_abc = abc[j];
-			if (temp_text == temp_abc)
-			{
-				*current = j;
-				exit = true;
-				break;
-			}
-		}
-	}
-}
-
-/* Запись битов */
-string write_bits(bool bit, int bits_to_foll)
-{
-	string temp;
-	temp += to_string(bit);
-	while (bits_to_foll > 0)
-	{
-		temp += to_string(!bit);
-		bits_to_foll -= 1;
-	}
-	return temp;
-}
-
-void output_file(string filename, string text)
-{
-	fstream file;
-	file.open(filename, ios::out);
-	for (int i = 0; i < text.size(); i++)
-	{
-		file << text[i];
-	}
-	file.close();
-}
-
-string encode_text(int *freq, string text, string abc)
-{
-	int mass_size = text.length();
-	unsigned short int *_low = new unsigned short int[mass_size];
-	unsigned short int *_high = new unsigned short int[mass_size];
-	_low[0] = _low_edge;
-	_high[0] = _high_edge;
-	int current = 1;	/* Какой элемент берём */
-	int i = 0;			/* Где находимся */
-	unsigned int range = 0;
-	/* Del - последняя накопленная частота */
-	int del = freq[abc.size() - 1];
-	int bits_to_foll = 0;
-	string code = "";
-
-	while (i < text.length())
-	{
-		get_next_symbol(text, abc, i, &current);
-		i += 1;
-
-		range = _high[i - 1] - _low[i - 1] + 1;
-		_low[i] = _low[i - 1] + (range*freq[current - 1])/del;
-		_high[i] = _low[i - 1] + (range*freq[current])/del - 1;
-		if (debug_mode)
-		{
-			cout << "[" << _low[i] << "; " << _high[i] << ")" << endl;
-		}
-
-		for (;;)
-		{
-			if (_high[i] < half)
-			{
-				code += write_bits(0, bits_to_foll);
-				bits_to_foll = 0;
-			}
-			else if (_low[i] >= half)
-			{
-				code += write_bits(1, bits_to_foll);
-				bits_to_foll = 0;
-				_low[i] -= half;
-				_high[i] -= half;
-			}
-			else if (_low[i] >= first_qtr && _high[i] < third_qtr)
-			{
-				bits_to_foll += 1;
-				_low[i] -= first_qtr;
-				_high[i] -= first_qtr;
-			}
-			else break;
-			_low[i] = 2 * _low[i];
-			_high[i] = 2 * _high[i] + 1;
-		}
-	}
-	if (debug_mode)
-	{
-		cout << endl << code << endl;
-	}
-	return code;
-}
-
-int to_int(string text, int _pos)
-{
-	int n = 0;
-	for (int i = _pos; i < sizeof(short)*8 + _pos; i++) 
-	{
-		n <<= 1;
-		n |= text[i] - '0';
-	}
-	return n;
-}
-
-string to_bits_16(int _value)
-{
-	bitset<16> a(_value); //convent number into bit array
-	string mystring = a.to_string<char, char_traits<char>, allocator<char> >();
-	return mystring;
-}
-
-int add_bit(int value, int count_taken, string text, bool &flag)
-{
-	/* Создаем битсет объекты */
-	bitset<16> a(value);
-	
-	/* Проверяем первый бит в буффере (если 1)*/
-	if (flag == 1)
-	{
-		a.reset(0);
-	}
-	else if (count_taken >= text.length())
-	{
-		a.set(0);
-		flag = 1;
-	}
-	else if (text[count_taken] == '1')
-	{
-		a.set(0);
-	}
-	else if (text[count_taken] == '0')
-	{
-		a.reset(0);
-	}
-	value = (unsigned short int)(a.to_ulong());
-	return value;
-}
-
-string decode(int *freq, string text, string abc)
-{
-	string decode_text = "";
-	int mass_size = text.length();
-	unsigned short int *_low = new unsigned short int[mass_size];
-	unsigned short int *_high = new unsigned short int[mass_size];
-	_low[0] = _low_edge;
-	_high[0] = _high_edge;
-
-	unsigned int range = 0;
-	unsigned int cum = 0;
-	int del = freq[abc.size() - 1];
-
-	unsigned short int value = to_int(text, 0);		// Забираем 16 бит в value
-	int count_taken = 16;
-
-	bool flag = 0;
-
-	for (int i = 1;; i++)
-	{
-		range = (_high[i - 1] - _low[i - 1]) + 1;
-		cum = (((value - _low[i - 1]) + 1) * del - 1) / range;
-		
-		int symbol;
-		for (symbol = 1; freq[symbol] <= cum; symbol++); //Другой знак
-
-		_low[i] = _low[i - 1] + (range * freq[symbol - 1]) / del;
-		_high[i] = _low[i - 1] + (range * freq[symbol]) / del - 1;
-
-		decode_text += abc[symbol];
-		if (debug_mode)
-		{
-			cout << "Symbol is: " << abc.at(symbol) << endl;
-			cout << "Value is: " << value << endl;
-			cout << "Current string is: " << decode_text << endl << endl;
-		}
-
-		if (abc[symbol] == symbol_break)
-		{
-			return decode_text;
-		}
-
-		for (;;)
-		{
-			if (_high[i] >= half)
-			{
-				if (_low[i] >= half)
-				{
-					value -= half;
-					_low[i] -= half;
-					_high[i] -= half;
-				}
-				else if(_low[i] >= first_qtr && _high[i] < third_qtr)
-				{
-					value -= first_qtr;
-					_low[i] -= first_qtr;
-					_high[i] -= first_qtr;
-				}
-				else
-				{
-					break;
-				}
-			}
-
-			_low[i] = 2 * _low[i];
-			_high[i] = 2 * _high[i] + 1;
-			value = add_bit(2*value, count_taken, text, flag);
-			count_taken++;
-		}
-	}
-}
-
-void test_function(string text)
-{
-	cout << endl << "TEST FUNCTION: " << endl;
-
-	unsigned short int value = to_int(text, 0);			//Забираем 2 байта
-	unsigned short int buffer = to_int(text, 16);
-	int bits_to_go = 0;
-
-
-	string _value = to_bits_16(value);
-	string _buffer = to_bits_16(buffer);
-	cout << "Value (0-15): " << _value << endl;
-	cout << "Buffer (16-31): " << _buffer << endl << endl;
-
-	cout << "ADD BIT WITH SLIDE: " << endl;
-	cout << "value: " << _value << endl;
-	value *= 2;
-	_value = to_bits_16(value);
-	cout << "value * 2: " << _value << endl;
-
-	/* Создаем битсет объекты */
-	bitset<16> a(value);
-	bitset<16> b(buffer);
-
-	/* Проверяем первый бит в буффере (если 1)*/
-	bool check = b.test(15);
-	if (check)
-	{
-		a.set(0);
-	}
-	else
-	{
-		a.reset(0);
-	}
-	value = (unsigned short int)(a.to_ulong());
-	_value = to_bits_16(value);
-	cout << "value *2 + new_bit: " << _value << endl;
-
-	//cout << "SLIDE: " << endl;
-	//cout << "Buffer (16-31): " << _buffer << endl;
-	//buffer *= 2;
-	//_buffer = to_bits_16(buffer);
-	//cout << "Buffer (16-31) * 2: " << _buffer << endl << endl;
-	//bitset<16> c(_buffer);
-	//c.reset(15);
-	//buffer = (unsigned short int)(c.to_ulong());
-	//_buffer = to_bits_16(buffer);
-	//cout << "Buffer (16-31) LAST: " << _buffer << endl << endl;
-}
 
 /* Generate random char[n] */
 void gen(char* p, size_t n)
@@ -380,34 +20,32 @@ void gen(char* p, size_t n)
 
 int main()
 {
-	//string text = get_file(filename_input);
 	for (int i = 0; i < test_count; i++)
 	{
 		char s[length_of_string + 1] = { 0 };
 		gen(s, length_of_string);
 		string text(s);
-
 		text.push_back(symbol_break);
-		string abc = get_abc(text);
-		int *freq = new int[abc.size()];
-		get_frequency(text, freq, abc);
 
-		if (debug_mode)
-		{
-			cout << text << endl;
-			cout << abc << endl;
-			for (int i = 0; i < abc.size(); i++)
-			{
-				cout << abc[i] << " - " << freq[i] << endl;
-			}
-			cout << endl;
-		}
+		//string text = "Hello, world! This method is better than Huffman. ";
+	
+		arithmetic coding;
+		coding.input(text);
+		coding.get_abc();
+		coding.get_frequency();
+		coding.encode_text();
 
-		string encoding = encode_text(freq, text, abc);
-		string decoding = decode(freq, encoding, abc);
+		string encode = coding.get_encode();
+		//coding.show_table();
+
+		coding.decode_text();
+		string decode = coding.get_decode();
+
+		//cout << "encode: " << encode << endl;
+		//cout << "decode: " << decode << endl;
 
 		cout << "Test #" << i << ": ";
-		if (text == decoding)
+		if (text == decode)
 		{
 			cout << "good";
 		}
